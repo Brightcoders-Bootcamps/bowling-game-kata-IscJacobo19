a3a5ce8e0f647426a68ade32efa38d84254ec09d
Manuel Jacobo <iscjacobomanuel@gmail.com>
2020-06-19 10:28:23 -0500

    code optimization  *Various methods were optimized  *Delete some methods, to have less code  *score 83.2

--- games.rb
+++ games.rb
@@ -1,58 +1,44 @@
-# The Games class is responsible for the entire game
+# Classe games
 class Games
   # The balls method, is that it reads the shots
   def balls(bowling)
+    frame = []
     counter_frame = 0
     ball = 0
-    frame = []
     score = 0
     while ball < 19
       turn = [bowling[ball], bowling[ball + 1], bowling[ball + 2], bowling[ball + 3], bowling[ball + 4]]
-      total = game_rules(turn, counter_frame)
-      score += total
-      frame[counter_frame] = score
+      score = frame[counter_frame] = save_score(turn, counter_frame, score)
       counter_frame += 1
-      ball += 2
-    end
+      ball += 2 end
     frame
+  end
+
+  # save score
+  def save_score(turn, counter_frame, score)
+    total = game_rules(turn, counter_frame)
+    score + total
   end
 
   # rules game
   def game_rules(turn, counter_frame)
     if counter_frame < 9
-      if turn[0] == 10
-        strike_game(turn[2], turn[3], turn[4])
-      elsif turn[1].positive? && turn[1] + turn[0] == 10
-        spare_game(turn[2])
-      else
-        normal_game(turn[0], turn[1])
+      gamebowling(turn)
+    else
+      (turn[0] + turn[1] + turn[2])
+    end
+  end
+
+  # Gamebowling
+  def gamebowling(turn)
+    if turn[0] == 10
+      if turn[2] == 10 && turn[4] == 10
+        30 else turn[2] + turn[3] + 10
       end
+    elsif turn[1].positive? && turn[1] + turn[0] == 10
+      turn[2] + 10
     else
-      last_game(turn[0], turn[1], turn[2])
+      turn[0] + turn[1]
     end
-  end
-
-  # Normal_game
-  def normal_game(first_ball, second_ball)
-    first_ball + second_ball
-  end
-
-  # Sapre_game
-  def spare_game(third_ball)
-    third_ball + 10
-  end
-
-  # Strike_game
-  def strike_game(third_ball, fourth_ball, fifth_ball)
-    if third_ball == 10 && fifth_ball == 10
-      30
-    else
-      third_ball + fourth_ball + 10
-    end
-  end
-
-  # last_game
-  def last_game(first_ball, second_ball, third_ball)
-    first_ball + second_ball + third_ball
   end
 end
